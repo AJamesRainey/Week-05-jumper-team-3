6b0bfbd6a4384e6dba0b11e1e5aac6a2b461315c
AJamesRainey <64215312+AJamesRainey@users.noreply.github.com>
2021-10-15 19:27:55 -0600

    added what I have so far for player.py

--- jumper/game/Player.py
+++ jumper/game/Player.py
@@ -1,9 +1,13 @@
 class player():
     def __init__(self):
         self.guesses = []
-        
+        self.letter = ""
 
     def player_input(self):
-        pass
-
+        letter = input("What is the letter?: ")
+        while True:
+            letter = input("Enter a letter: ")
+            if len(self.letter) < 2 and letter not in self.guesses:
+                break
+            self.guesses.append(self.letter)
 
